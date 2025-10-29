d99e033014c05919fedd609b14d7d290e7df5e56
codejordy25 <jordytsimba11@gmail.com>
2025-10-29 14:27:43 +0100

    287fetching data with react routeur laoders

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,17 @@
+import { formatCurrency } from "../../utils/helpers";
+
+import PropTypes from "prop-types";
+
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
+  console.log("pizzaId:", id);
   return (
     <li>
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
@@ -14,5 +19,17 @@ function MenuItem({ pizza }) {
     </li>
   );
 }
+
+// Validation des props
+MenuItem.propTypes = {
+  pizza: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    unitPrice: PropTypes.number.isRequired,
+    ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+    soldOut: PropTypes.bool.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MenuItem;
