7f5232a174139d349ff5e33d474331390919a62c
codejordy25 <jordytsimba11@gmail.com>
2025-10-30 15:26:12 +0100

    Fix 288 289 290 Fetch Orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
 
+//ici il s'agit d'une importation nommée au Niveau menu et Order
+import Menu, { Loader as menuLoader } from "./features/menu/Menu";
+import Order, { Loader as orderLoader } from "./features/order/Order";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+
+import Home from "./ui/Home";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+//on peut placer le error element dans le layout pour catcher les erreurs des enfants
+//Ou n'importe où dans l'arborescence des routes
+//On peut le definir aussi sur chaque route individuellement
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />, //Outlet with child , #"my bug is elemment  "
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -19,10 +27,16 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
