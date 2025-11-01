be11c8fdeef5e024d77ee47e4161c9310225361c
codejordy25 <jordytsimba11@gmail.com>
2025-11-01 11:15:06 +0100

    Styling Text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
     <div>
-      <header className="bg-yellow-500" >
-        <Link to="/">Fast React Pizza Co.</Link>
+      <header className="bg-yellow-500 uppercase" >
+        <Link className="tracking-widest" to="/">Fast React Pizza Co.</Link>
         <SearchOrder />
-        <p>Jordy</p>
+         <Username/> 
       </header>
     </div>
   );
