70ffd6e0f276995f3fb6bc2967d3e311e94e10d9
codejordy25 <jordytsimba11@gmail.com>
2025-11-01 12:25:44 +0100

    Spacing Boders, display

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username";
 function Header() {
   return (
     <div>
-      <header className="bg-yellow-500 uppercase" >
+      <header className="border-b-2 border-stone-300 px-4 py-3 bg-yellow-500 uppercase" >
         <Link className="tracking-widest" to="/">Fast React Pizza Co.</Link>
         <SearchOrder />
          <Username/> 
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,8 @@
+import CreateUser from "../features/user/CreateUser";
 function Home() {
   return (
-    <div>
-      <h1 className="text-xl font-semibold text-center">
+    <div className="my-10 text-center" >
+      <h1 className="mb-8 text-xl font-semibold">
         The best pizza.
         <br />
         <span className="text-yellow-500">
@@ -9,6 +10,7 @@ function Home() {
         Straight out of the oven, straight to you.
         </span>
       </h1>
+      <CreateUser/>
     </div>
   );
 }
